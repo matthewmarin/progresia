20680613158eee0622585e15d6166a9b2345567c
Matthew Rich Marin <matthewrichmarin@gmail.com>
2023-08-02 10:04:40 +0800

    adding darkmode function, logo, and header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Header from "./components/Header";
+import Center from "./components/Center";
 
 function App() {
-  return <div>Hello WOrld</div>;
+  return (
+    <div>
+      {/* Header Section */}
+
+      <Header />
+
+      {/* Center Section */}
+
+      <Center />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import logo from "../assets/logo.svg";
+import iconDown from "../assets/icon-chevron-down.svg";
+import iconUp from "../assets/icon-chevron-up.svg";
+
+function Header() {
+  const [openDropdown, setOpenDropdown] = useState(false);
+  return (
+    <div className="p-4 fixed left-0 bg-white dark:bg-[#2b2c37] z-50 right-0">
+      <header className="flex justify-between dark:text-white items-center">
+        {/* Left Side */}
+
+        <div className="flex items-center space-x-2 md:space-x-4">
+          <img src={logo} alt="logo" className="h-[100px] w-[300px]" />
+          <div className="flex items-center">
+            <img
+              src={openDropdown ? iconUp : iconDown}
+              alt="dropdown icon"
+              className="w-3  cursor-pointer md:hidden"
+              onClick={() => setOpenDropdown((state) => !state)}
+            />
+          </div>
+        </div>
+
+        {/* Right Side */}
+
+        <div className="flex space-x-4 items-center md:space-x-6">
+          <button className="button">+ Add New task</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;

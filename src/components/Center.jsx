4e76bf019fb7f140633c17e0897072c9473aba1a
Matthew Rich Marin <matthewrichmarin@gmail.com>
2023-08-09 15:17:12 +0800

    adding function to add more columns

--- src/components/Center.jsx
+++ src/components/Center.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 import { useSelector } from "react-redux";
 import Column from "./Column";
+import EmptyBoard from "./EmptyBoard";
+import AddEditBoard from "../modal/AddEditBoard";
 
 function Center({ boardModalOpen, setBoardModalOpen }) {
   const [windowSize, setWindowSize] = useState([
@@ -37,9 +39,31 @@ function Center({ boardModalOpen, setBoardModalOpen }) {
       {windowSize[0] >= 768 && <Sidebar />}
 
       {/* Column Section */}
-      {columns.map((col, index) => (
-        <Column key={index} colIndex={index} />
-      ))}
+      {columns.length > 0 ? (
+        <>
+          {columns.map((col, index) => (
+            <Column key={index} colIndex={index} />
+          ))}
+          <div
+            onClick={() => {
+              setBoardModalOpen(true);
+            }}
+            className="h-screen dark:bg-[#2b2c3740] flex justify-center items-center
+      font-bold text-2xl hover:text-[#d8c648] dark:hover:text-[#33c6d8] transition duration-300
+      cursor-pointer bg-[#e9effa] scrollbar-hide mb-2 mx-5 pt-[90px] min-w-[280px]
+      text-[#828fa3] mt-[135px] rounded-lg"
+          >
+            + New Column
+          </div>
+        </>
+      ) : (
+        <>
+          <EmptyBoard type="edit" />
+        </>
+      )}
+      {boardModalOpen && (
+        <AddEditBoard type="edit" setBoardModalOpen={setBoardModalOpen} />
+      )}
     </div>
   );
 }
--- src/redux/boardsSlice.js
+++ src/redux/boardsSlice.js
@@ -54,29 +54,6 @@ const boardsSlice = createSlice({
       column.tasks.push(task);
     },
 
-    editTask: (state, action) => {
-      const {
-        title,
-        status,
-        description,
-        subtasks,
-        prevColIndex,
-        newColIndex,
-        taskIndex,
-      } = action.payload;
-      const board = state.find((board) => board.isActive);
-      const column = board.columns.find((col, index) => index === prevColIndex);
-      const task = column.tasks.find((task, index) => index === taskIndex);
-      task.title = title;
-      task.status = status;
-      task.description = description;
-      task.subtasks = subtasks;
-      if (prevColIndex === newColIndex) return;
-      column.tasks = column.tasks.filter((task, index) => index !== taskIndex);
-      const newCol = board.columns.find((col, index) => index === newColIndex);
-      newCol.tasks.push(task);
-    },
-
     // editTask: (state, action) => {
     //   const {
     //     title,
@@ -87,39 +64,62 @@ const boardsSlice = createSlice({
     //     newColIndex,
     //     taskIndex,
     //   } = action.payload;
-
-    //   const updatedTask = {
-    //     title,
-    //     status,
-    //     description,
-    //     subtasks,
-    //   };
-
-    //   const newState = state.map((board) =>
-    //     board.isActive
-    //       ? {
-    //           ...board,
-    //           columns: board.columns.map((col, index) =>
-    //             index === prevColIndex
-    //               ? {
-    //                   ...col,
-    //                   tasks: col.tasks.map((task, i) =>
-    //                     i === taskIndex ? updatedTask : task
-    //                   ),
-    //                 }
-    //               : index === newColIndex
-    //               ? {
-    //                   ...col,
-    //                   tasks: [...col.tasks, updatedTask],
-    //                 }
-    //               : col
-    //           ),
-    //         }
-    //       : board
-    //   );
-
-    //   return newState;
+    //   const board = state.find((board) => board.isActive);
+    //   const column = board.columns.find((col, index) => index === prevColIndex);
+    //   const task = column.tasks.find((task, index) => index === taskIndex);
+    //   task.title = title;
+    //   task.status = status;
+    //   task.description = description;
+    //   task.subtasks = subtasks;
+    //   if (prevColIndex === newColIndex) return;
+    //   column.tasks = column.tasks.filter((task, index) => index !== taskIndex);
+    //   const newCol = board.columns.find((col, index) => index === newColIndex);
+    //   newCol.tasks.push(task);
     // },
+
+    editTask: (state, action) => {
+      const {
+        title,
+        status,
+        description,
+        subtasks,
+        prevColIndex,
+        newColIndex,
+        taskIndex,
+      } = action.payload;
+
+      const updatedTask = {
+        title,
+        status,
+        description,
+        subtasks,
+      };
+
+      const newState = state.map((board) =>
+        board.isActive
+          ? {
+              ...board,
+              columns: board.columns.map((col, index) =>
+                index === prevColIndex
+                  ? {
+                      ...col,
+                      tasks: col.tasks.map((task, i) =>
+                        i === taskIndex ? updatedTask : task
+                      ),
+                    }
+                  : index === newColIndex
+                  ? {
+                      ...col,
+                      tasks: [...col.tasks, updatedTask],
+                    }
+                  : col
+              ),
+            }
+          : board
+      );
+
+      return newState;
+    },
 
     setSubtaskCompleted: (state, action) => {
       const payload = action.payload;
@@ -146,6 +146,13 @@ const boardsSlice = createSlice({
       const board = state.find((board) => board.isActive);
       const col = board.columns.find((col, i) => i === payload.colIndex);
       col.tasks = col.tasks.filter((task, i) => i !== payload.taskIndex);
+    },
+    dragTask: (state, action) => {
+      const { colIndex, prevColIndex, taskIndex } = action.payload;
+      const board = state.find((board) => board.isActive);
+      const prevCol = board.columns.find((col, i) => i === prevColIndex);
+      const task = prevCol.tasks.splice(taskIndex, 1)[0];
+      board.columns.find((col, i) => i === colIndex).tasks.push(task);
     },
   },
 });
